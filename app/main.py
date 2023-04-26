8d49eb89ac59f8812152de0bc93905112d678cfc
huyjangvn <huyjangvn@gmail.com>
2023-04-26 14:37:56 +0700

    add middleware for process time

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+import time
+from fastapi import FastAPI, Request
 from routers import company, user, task, auth
 
 app = FastAPI()
@@ -12,3 +13,12 @@ app.include_router(auth.router)
 @app.get("/")
 async def health_check():
     return "API Service is up and running!"
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
